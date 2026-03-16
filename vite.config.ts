ad5d1d40bee77f8a48204abdb6dac3b7871511aa
Adam yusuf <yuusufady2000@gmail.com>
2026-03-16 13:04:43 +0100

    Fix Vercel output folder for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
+// vite.config.ts
 import { defineConfig } from "vite";
+import reactRouter from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  build: {
+    outDir: "build"
+  }
 });
