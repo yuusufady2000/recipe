a9b04493946951cdf8e6e980865506daab39ee62
Adam yusuf <yuusufady2000@gmail.com>
2026-03-16 13:52:20 +0100

    Fix Vite config for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
-// vite.config.ts
 import { defineConfig } from "vite";
-import reactRouter from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), tsconfigPaths()],
   build: {
     outDir: "build"
   }
